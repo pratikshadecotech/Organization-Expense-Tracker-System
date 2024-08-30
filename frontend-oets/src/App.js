5f1f1a7bfa65d363428c8188e581f92d473ef728
pratikshanhts2 <backenddevelopernhts2@gmail.com>
2024-08-31 00:24:00 +0530

    FE code started- date-31082024

---
+++ frontend-oets/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import RegisterForm from '../src/components/User/RegisterForm.js';
+
+function App() {
+  return (
+    <>
+      <RegisterForm />
+      {/* <RegisterFormList/> */}
+    </>
+
+  );
+}
+
+export default App;
